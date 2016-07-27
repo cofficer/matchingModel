f75385416a23ce4db3d02cc4bd0197144728eb74
cofficer <christoffer.gahnstrom@hotmail.com>
2016-07-27 12:18:00 +0200

    not sure about changes

--- plot_behavior.m
+++ plot_behavior.m
@@ -1,6 +1,6 @@
-%Plot behavior
 
-
+ 
+ 
 %%
 
 
@@ -44,23 +44,46 @@ xlim([0.7 2.3])
 
 
 %%
-%Plot individually
+%Calulate maximulu likelihood estimation
+%Load in all the parameters estimations for all participants and
+%calculate the averages over all runs. 
 
 %Struct with all the trial likelihoods calculated. 
+cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/analysis/matchingModel/results')
+parts=dir('*.mat');
 
+allParams=zeros(length(parts),2,2); %tau/beta, pla/atm. 1 tau, 1 pla
+
+for ipart = 1:length(parts)
+
+    load(parts(ipart).name)
+    
 %Placebo
 plaMLE = allMLE.PLA;
+atmMLE = allMLE.ATM;
 
+if ipart == 1
+    
+tableTBplaAll     = zeros(size(plaMLE,3),size(plaMLE,2),length(parts));
+
+
+tableTBatmAll     = zeros(size(plaMLE,3),size(plaMLE,2),length(parts));
+end
 
 tauAll=0;
 
-tableTB     = zeros(size(plaMLE,3),size(plaMLE,2));
+tableTBpla     = zeros(size(plaMLE,3),size(plaMLE,2));
+
+
+tableTBatm     = zeros(size(plaMLE,3),size(plaMLE,2));
+
+
 
 
 
 for eachBeta = 1:size(plaMLE,3)
     for eachTau = 1:size(plaMLE,2)
-        for eachrun = 1:2%size(plaMLE,4)
+        for eachrun = 1:size(plaMLE,4)
             
             
             
@@ -68,9 +91,16 @@ for eachBeta = 1:size(plaMLE,3)
             %Per beta and run, the product over all the different tau and
             %choosing the maximum.
             %[~,maxDis]=max(prod(plaMLE(:,eachTau,eachBeta,eachrun)));
-            MLE = prod(plaMLE(:,eachTau,eachBeta,eachrun));
+            MLEpla = prod(plaMLE(:,eachTau,eachBeta,eachrun));
+            MLEatm = prod(atmMLE(:,eachTau,eachBeta,eachrun));
 
-            tableTB(eachBeta,eachTau) = tableTB(eachBeta,eachTau)+MLE;
+            
+            
+            tableTBpla(eachBeta,eachTau) = tableTBpla(eachBeta,eachTau)+MLEpla;
+            tableTBatm(eachBeta,eachTau) = tableTBatm(eachBeta,eachTau)+MLEatm;
+            
+            
+
             
             %tauAll(eachrun,eachBeta)=tau(maxDis);
             
@@ -80,27 +110,85 @@ for eachBeta = 1:size(plaMLE,3)
     end
     %tableTB(eachTau,eachBeta)=tableTB(eachTau,eachBeta)./10
 end
+            tableTBplaAll(:,:,ipart) = tableTBpla;
+            tableTBatmAll(:,:,ipart) = tableTBatm;
+% 
+% x=linspace(2,10,20);
+% y=linspace(1,5,20);
+% imagesc(x,y,(mean(tableTBplaAll,3)))
+% set(gca,'YDir','normal')
+% colorbar
 
 
-x=cfg1{1}.tau;
-y=cfg1{1}.beta;
-imagesc(x,y,log(tableTB))
-set(gca,'YDir','normal')
-colorbar
+[valpla,indpla]=max(tableTBpla(:));
+[rowpla,colpla]=ind2sub(size(tableTBpla),indpla);
 
+currTauPla = linspace(2,10,20);
+currTauPla=currTauPla(colpla);%cfg1{1}.tau(colpla);
+currBetaPla =linspace(1,5,20);
+currBetaPla=currBetaPla(rowpla); %cfg1{1}.beta(rowpla);
+
+[valatm,indatm]=max(tableTBatm(:));
+[rowatm,colatm]=ind2sub(size(tableTBatm),indatm);
+
+currTauAtm = linspace(2,10,20);
+currTauAtm=currTauAtm(colatm);%cfg1{1}.tau(colatm);
+currBetaAtm =  linspace(1,5,20);
+currBetaAtm=currBetaAtm(rowatm); %cfg1{1}.beta(rowatm)
+
+allParams(ipart,1,1) = currTauPla;
+allParams(ipart,2,1) = currBetaPla;
+allParams(ipart,1,2) = currTauAtm;
+allParams(ipart,2,2) = currBetaAtm;
+
+disp(ipart)
+
+end
+
+disp('done')
+
+
+
+for ir = 1:200
+MLEpla(1,ir) = log(prod(plaMLE(:,2,2,ir)));
+end
 
 %%
-plot(prod((mean(plaMLE(:,:,10,:),4)),1),'r')
+%plot(prod((mean(plaMLE(:,:,10,:),4)),1),'r')
 
 
 
 
 
+figure(1),clf
+hold on;
+
+%Each condition
+at=squeeze(allParams(:,1,2));
+pl=squeeze(allParams(:,1,1));
+
+%Mean each condition
+atAV=nanmean(at,1);
+plAV=nanmean(pl,1);
+
+bar([1 2],[nanmean(plAV) nanmean(atAV)])
+
+errorbar([1 2],[(plAV) (atAV)],[std(pl) std(at)])
+
+%barwitherr([std(atAV(1:end-1)) std(plAV)],[nanmean(atAV) nanmean(plAV)])
+
+
+plot([1,2],[pl at])
+xlim([0.7 2.3])
 
 
 
 
+%% Save figure, data
+cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/analysis/TFGitAnlysis/figures')
+print('TauParamBARv2','-depsc2')
 
+csvwrite('both.csv',both)
 
 
 
--- softmaxOwn.m
+++ softmaxOwn.m
@@ -4,7 +4,7 @@ function [output]=softmaxOwn(input,beta)
 %Softmax decision rule instead of matching law.
 %Input the difference value based on the two local incomes.
 %Output the 
-xval=linspace(-1,1,100);
+%xval=linspace(-0.5,0.5,100);
 
 output=1/(1+exp(-(input)*beta));
 
@@ -12,7 +12,7 @@ output=1/(1+exp(-(input)*beta));
 % figure(1),clf
 % hold on;
 % for i =1:100
-%     plot(xval(i),1/(1+exp(-xval(i)*beta)),'o')
+%     plot(xval(i),1/(1+exp(-xval(i)*5)),'o')
 % end
 
 end
