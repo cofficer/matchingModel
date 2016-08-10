72716434bc43fe9cdf8a0a0fc6613e0f36eea543
cofficer <christoffer.gahnstrom@hotmail.com>
2016-08-10 15:12:23 +0200

    Made quite some cleaning up, removal of old commented lines of code

--- Model_performance.m
+++ Model_performance.m
@@ -1,5 +1,5 @@
 
-function Model_performance(cfg1,outputfile)
+function [allMLE,choiceStreamATM,choiceStreamPLA]=Model_performance(cfg1,outputfile)
 %Main function to run for the leaky integration model. 
 %Model performance should be based on X trials with block conditions identical to those
 %made by the participant.
@@ -9,27 +9,6 @@ tic
 cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/All_behavior')
 
 
-
-
-%What is the purpose of missm?
-missm   = 0;
-
-
-%Placeholders for storing foraging efficiency
-numDelivered=1;
-numVa=1;
-
-% %Preallocating: Stores the rewards delivered.
-% allDelivered=zeros(1,length(tau),runs+length(structfun(@numel,resultsAll)));
-% lowTauAll=zeros(1,length(structfun(@numel,resultsAll)),runs);
-% maxTauAll=zeros(1,length(structfun(@numel,resultsAll)),runs);
-% forEffAll=zeros(1,length(structfun(@numel,resultsAll)));
-% AUCAll=zeros(1,length(tau),length(structfun(@numel,resultsAll)));
-
-
-%%
-
-
 for allSessions=1:2
     
     if allSessions == 1 %load placebo
@@ -37,6 +16,8 @@ for allSessions=1:2
 
     else                %load atomoxetine
         load(cfg1.ATMpath)
+        
+        %Store choice probabilities for placebo session.
         allMLE.PLA=modelChoiceP;
         choiceStreamPLA = choiceStreamAll;
         
@@ -44,36 +25,23 @@ for allSessions=1:2
     end
     
     
-    %For the output choiceStreamAll, 0 is horizontal and 1 is vertical
-    [choiceStreamAll,rewardStreamAll] = global_matching(results);
-    
-    Totaltrials=length(choiceStreamAll);%results.parameters.trlTotal;
-    Totalblocks=length(results.blocks); %Changed 2/july
-    
-    %Pre-allocating matrices for optimality.
-    mean_sq=zeros(1,length(cfg1.tau),cfg1.runs);
-    mean_sqr=zeros(1,length(cfg1.tau),cfg1.runs);
-    rewardCount=zeros(1,length(cfg1.tau),length(cfg1.beta),cfg1.runs);
-    modelChoiceVerComp=zeros(1,Totaltrials,length(cfg1.tau));
-    max_like=zeros(Totaltrials,length(cfg1.tau),length(cfg1.beta),cfg1.runs);
-    
-    %Function model_body does the model-specific computations.
-    [rewardCount,modelChoiceP] = model_body( cfg1,results,choiceStreamAll,rewardStreamAll,rewardCount);
-        
+    %Get the history of choices and rewards, 0 is horizontal and 1 is vertical
+    [choiceStreamAll,rewardStreamAll] = global_matching(results);        
    
-    
+    %Model-specific computations.
+    [modelChoiceP] = model_body( cfg1,results,choiceStreamAll,rewardStreamAll);
+        
     
 end
 
-
-%plot(allSessions,mean(maxTauAll),'o')
+%Store choice probabilities for atomoxetine session.
 allMLE.ATM = modelChoiceP;
 
 choiceStreamATM = choiceStreamAll;
 
 timeTaken=toc;
 %Saving all the relevant data per participant.
-save(outputfile,'allMLE','timeTaken','choiceStreamPLA','choiceStreamATM');%'maxTauAll','allMLE','forEffAll','allDelivered','missm','timeTaken')
+%save(outputfile,'allMLE','timeTaken','choiceStreamPLA','choiceStreamATM');%'maxTauAll','allMLE','forEffAll','allDelivered','missm','timeTaken')
 
 
 disp('model done')
